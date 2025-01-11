187a076f675fbaf3651c18440d213bbfbdc4c534
John McIver <john.mciver.iii@gmail.com>
2025-01-10 19:58:28 -0700

    Remove special handling of build based targets

--- install-profiles.sh
+++ install-profiles.sh
@@ -16,28 +16,6 @@ then
 	 --max `cpupower frequency-info | grep "hardware limits" | awk '{print $6,$7}' | tr -d ' '`
 fi
 
-# Omit the profiles in LLVM Build Speed as they need special treatment
-for p in $(grep -v '#' categorized-profiles.txt | grep -v '/build-')
-do
-	$PTS debug-install $p 2>&1 | tee $LOG_DIR/$p.log
+for p in $(grep -v '#' categorized-profiles.txt); do
+    $PTS debug-install $p 2>&1 | tee $LOG_DIR/$p.log
 done
-
-# Process the profiles in LLVM Build Speed
-if [[ $(grep -v '#' categorized-profiles.txt | grep '/build-' | wc -l) -gt 0 ]]
-then
-	COMPILED_CLANG_PATH=`pwd`/llvm-project-llvmorg-15.0.7
-	if [[ `lscpu | grep -ic x86` = 1 ]]
-	then
-		(cd $COMPILED_CLANG_PATH && rm -rf build/ && cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PROJECTS="llvm;clang" -S ./llvm -B build/ && ninja -C build)
-	else
-		(cd $COMPILED_CLANG_PATH && rm -rf build/ && cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=AArch64 -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PROJECTS="llvm;clang" -S ./llvm -B build/ && ninja -C build)
-	fi
-
-	PTS_COMMAND="(trap 'kill 0' INT; "
-	for p in $(grep -v '#' categorized-profiles.txt | grep '/build-')
-	do
-		PTS_COMMAND=$PTS_COMMAND"PATH='$COMPILED_CLANG_PATH/build/bin:$PATH' CC=$COMPILED_CLANG_PATH/build/bin/clang CXX=$COMPILED_CLANG_PATH/build/bin/clang++ \$PTS debug-install $p 2>&1 | tee \$LOG_DIR/$p.log & "
-	done
-	PTS_COMMAND=$PTS_COMMAND"wait)"
-	eval $PTS_COMMAND
-fi
--- run-profiles.sh
+++ run-profiles.sh
@@ -25,23 +25,11 @@ then
     fi
 fi
 
-for p in $(grep -v '#' categorized-profiles.txt | grep -v '/build-')
-do
-	result_name=`echo $p | cut -d'/' -f2`"$@"
-	result_name="$result_name\n$result_name\n$result_name"
-	pts_command="echo -n '$result_name' | $PTS batch-run $p"
-	sh -c "$pts_command" 2>&1| tee -a $LOG_FILE
-done
-
-# CC=`pwd`/llvm-project-llvmorg-15.0.7/build/bin/clang
-# CXX=`pwd`/llvm-project-llvmorg-15.0.7/build/bin/clang++
-
-for p in $(grep -v '#' categorized-profiles.txt | grep '/build-')
-do
-	result_name=`echo $p | cut -d'/' -f2`"$@"
-	result_name="$result_name\n$result_name\n$result_name"
-	pts_command="echo -n '$result_name' | $PTS batch-run $p"
-	sh -c "$pts_command" 2>&1| tee -a $LOG_FILE
+for p in $(grep -v '#' categorized-profiles.txt); do
+    result_name=`echo $p | cut -d'/' -f2`"$@"
+    result_name="$result_name\n$result_name\n$result_name"
+    pts_command="echo -n '$result_name' | $PTS batch-run $p"
+    sh -c "$pts_command" 2>&1| tee -a $LOG_FILE
 done
 
 if [[ `lscpu | grep -ic arm` = 1 ]]
